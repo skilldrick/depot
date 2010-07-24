5286efc53863451800d938905fe3598bffea3f59
skilldrick <skilldrick@gmail.com>
2010-07-25 00:23:09 +0100

    Finished chapter 6.

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,8 @@ class Product < ActiveRecord::Base
   validates_numericality_of :price
   validate :price_must_be_at_least_a_cent
   validates_uniqueness_of :title
+  validates_length_of :title, :minimum => 10,
+  :message => 'must be longer than 10 characters.'
   validates_format_of :image_url,
   :with => %r{\.(gif|jpg|png)$}i,
   :message => 'must be a URL for GIF, JPG or PNG image.'
