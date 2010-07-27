82349df1b1dfa8d837bed22a6761492163cc3c9c
skilldrick <skilldrick@gmail.com>
2010-07-27 23:30:00 +0100

    Finished chapter 10 (except for the silly stuff about putting card
    details in the db)

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -35,6 +35,30 @@ class StoreController < ApplicationController
       format.js if request.xhr?
       format.html {redirect_to_index}
     end
+  rescue ActiveRecord::RecordNotFound
+    logger.error("Attempt to access invalid product #{params[:id]}")
+    redirect_to_index "Invalid product"
+  end
+
+  def checkout
+    @cart = find_cart
+    if @cart.items.empty?
+      redirect_to_index("Your cart is empty")
+    else
+      @order = Order.new
+    end
+  end
+
+  def save_order
+    @cart = find_cart
+    @order = Order.new(params[:order])
+    @order.add_line_items_from_cart(@cart)
+    if @order.save
+      session[:cart] = nil
+      redirect_to_index "Thank you for your order"
+    else
+      render :action => 'checkout'
+    end
   end
 
   private
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,32 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100725102609) do
+ActiveRecord::Schema.define(:version => 20100727215625) do
+
+  create_table "line_items", :force => true do |t|
+    t.integer  "product_id",                                :null => false
+    t.integer  "order_id",                                  :null => false
+    t.integer  "quantity",                                  :null => false
+    t.decimal  "total_price", :precision => 8, :scale => 2, :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "orders", :force => true do |t|
+    t.string   "name"
+    t.text     "address"
+    t.string   "email"
+    t.string   "pay_type",   :limit => 10
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "pay_types", :force => true do |t|
+    t.string   "name"
+    t.string   "value"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "products", :force => true do |t|
     t.string   "title"
