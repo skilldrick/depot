8bf791d19caebd64c3590a03d572f5ffa29c113a
skilldrick <skilldrick@gmail.com>
2010-07-31 00:11:20 +0100

    Finished chapter 12 except for playtime.

---
+++ app/controllers/info_controller.rb
@@ -0,0 +1,16 @@
+class InfoController < ApplicationController
+  def who_bought
+    @product = Product.find(params[:id])
+    @orders = @product.orders
+    respond_to do |format|
+      format.html
+      format.xml { render :layout => false }
+      format.atom { render :layout => false }
+      format.json { render :layout => false,
+        :json => @product.to_json(:include => :orders) }
+    end
+  end
+
+  def authorize
+  end
+end
