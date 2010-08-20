71adfd6366bfa5394543af103c8514f021ae1c87
skilldrick <skilldrick@gmail.com>
2010-08-20 20:49:20 +0100

    Finished testing chapter.

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,8 +1,10 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  test "authenticate" do
+    dave = users(:dave)
+    assert User.authenticate(dave.name, 'secret')
   end
+
 end
